1952105ee6e74ac54ca756acf50a409ca16a1579
CARLOS RENANZIN <carlos.barros71@aluno.ce.gov.br>
2026-03-31 15:57:02 +0000

    renen programador

--- 1-bimestre/programacao-web/ex14-enviando-valores-do-dom/src/js/script.js
+++ 1-bimestre/programacao-web/ex14-enviando-valores-do-dom/src/js/script.js
@@ -1,8 +1,7 @@
-const inputNome = document.getElementById("nome");
+const input = document.getElementById("nome");
 const botao = document.getElementById("mostrar");
 const resultado = document.getElementById("resultado");
 
 botao.addEventListener("click", () => {
-  const nomeDigitado = inputNome.value;
-  resultado.textContent = `Olá, ${nomeDigitado}`;
+  resultado.textContent = `Olá, ${input.value}`;
 });
